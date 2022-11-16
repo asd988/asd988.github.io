d9cf5b7097aeec6d7b8601a824ea3247477fe97c
BuildTools <enderhd.98@gmail.com>
2022-11-16 18:00:20 +0100

    dropdowns and search settings work

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,18 +1,31 @@
 import { useState } from "react";
 import { ReactComponent as SettingsIcon } from "../svgs/settings.svg"
+import { Dropdown } from "./Dropdown";
 
 import { Playlists } from "./Playlists"
 import { SearchBar } from "./SearchBar"
 
 export const Dashboard = () => {
   const [searchQuery, setQuery] = useState("");
+  const [sort, setSort] = useState(undefined);
+  const [selFilter, setSelFilter] = useState(undefined);
 
   return (
     <div className="h-4/5 md:border-[#aaa] md:border-solid md:border rounded-3xl w-full flex-grow md:mt-14 md:mb-14 p-6 flex flex-col items-center">
       <div className="flex">
-        <SearchBar setQuery={setQuery}></SearchBar>
+        <SearchBar setQuery={setQuery}></SearchBar> 
+        <Dropdown tag="Sort" setOptionId={setSort} options={[
+          {id: "default", display: "Default"},
+          {id: "a-z", display: "A to Z"},
+          {id: "z-a", display: "Z to A"}
+        ]}/>
+        <Dropdown tag="Filter" setOptionId={setSelFilter} options={[
+          {id: "none", display: "None"},
+          {id: "selected", display: "Selected"},
+          {id: "unselected", display: "Unselected"} 
+        ]}/>
       </div>
-      <Playlists searchQuery={searchQuery}></Playlists>
+      <Playlists searchQuery={searchQuery} sort={sort} selectionFilter={selFilter}></Playlists>
       <div className="bg-spotify rounded-full w-fit flex px-4 py-2 h-9 items-center">
         <button>Create Playlist</button>
         <button className="h-full aspect-square fill-[#eee] border-r-[#eee] border-l pl-2 ml-2 box-content">
---
+++ src/components/Dropdown.jsx
@@ -0,0 +1,30 @@
+import { useState } from "react";
+import OutsideClickHandler from "react-outside-click-handler";
+
+export const Dropdown = ({ tag = "", options, defaultIndex = 0, setOptionId = (i) => {} }) => {
+  const [isOpen, setOpen] = useState(false);
+  const [selectedOption, setSelectedOption] = useState(defaultIndex);
+
+  const changeOption = (i) => {
+    setSelectedOption(i)
+    setOptionId(options[i].id)
+  }
+
+  return (
+    <OutsideClickHandler onOutsideClick={() => setOpen(false)}>
+      <div className="flex flex-col items-center">
+        <button type="button" onClick={() => setOpen(!isOpen)}
+          className="fill-[#aaa] rounded-full border border-[#aaa] text-[#aaa] flex py-1 px-2 items-center font-light transition-all ml-2">
+          {tag + ": " + options[selectedOption].display}
+        </button>
+        <div className={"z-10 w-fit absolute mt-10 border-[#aaa] border text-[#aaa] rounded-2xl px-2 py-1 bg-[#0f0f0f] font-light" + (isOpen ? " flex flex-col" : " hidden")}>
+          {
+            options.map(({ display }, i) =>
+              <button key={i} onClick={() => changeOption(i)} className={(selectedOption == i) ? "font-normal text-[#eee]" : ""}>{display}</button>
+            )
+          }
+        </div>
+      </div>
+    </OutsideClickHandler>
+  )
+}
